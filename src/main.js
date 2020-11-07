10e68379e2833271d259c37807fb0c1966045089
ksenkso <yazunya@gmail.com>
2020-11-07 16:39:42 +0300

    fix: enable devtools on production

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import './styles/_common.scss';
 Vue.config.productionTip = false
-
+Vue.config.devtools = true;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
