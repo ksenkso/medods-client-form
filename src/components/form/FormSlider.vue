7be71c4a5f211273e50187160d3fed58a3f74958
ksenkso <yazunya@gmail.com>
2020-11-08 16:01:15 +0300

    fix: set tabindex to make browser follow the default tab order

--- src/components/form/FormSlider.vue
+++ src/components/form/FormSlider.vue
@@ -125,7 +125,7 @@ export default {
           })
         } else {
           controls.forEach(control => {
-            control.tabIndex = 1;
+            control.tabIndex = 0;
           });
         }
       })
